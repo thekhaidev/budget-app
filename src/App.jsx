b89067101a2bcf141284b0f15c9eb75f147a1c98
solo917 <kqtran917@yahoo.com>
2023-01-10 21:25:17 -0600

    Cloud merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 
-import Box from "@mui/material/Box";
+import {
+  Container,
+  Box,
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+  OutlinedInput,
+} from "@mui/material";
 
 const App = () => {
   return (
-    <Box>
-      <h1>Hello, world</h1>
+    <Box style={{ border: "1px solid red" }}>
+      <Container
+        style={{ textAlign: "center", border: "1px solid black" }}
+        fixed
+      >
+        <h1>Kimmy's Checkbook</h1>
+        <h3>Log In</h3>
+        <Box mb={2} component="form" noValidate autoComplete="off">
+          <FormControl sx={{ width: "25ch" }}>
+            <OutlinedInput name="email" placeholder="Email" />
+          </FormControl>
+        </Box>
+      </Container>
     </Box>
   );
 };
