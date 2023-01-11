4794ad79396c08d9db56c29b8590c7caa2ba1c4c
solo917 <kqtran917@yahoo.com>
2023-01-10 18:48:44 -0600

    Installed material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const App = () =>  {
-    return (
-        <h1> Hello World </h1>
-    )
-}
+const App = () => {
+  return <h1> Hello World </h1>;
+};
 
-export default App 
+export default App;
