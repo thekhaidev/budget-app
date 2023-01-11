b28f81c2c4ceccd8223f9add1a22ffd792b70117
solo917 <kqtran917@yahoo.com>
2023-01-10 22:19:58 -0600

    Fixed linter implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 import {
   Container,
@@ -8,25 +8,25 @@ import {
   Input,
   FormHelperText,
   OutlinedInput,
-} from "@mui/material";
+} from '@mui/material';
 
-const App = () => {
+function App() {
   return (
-    <Box style={{ border: "1px solid red" }}>
+    <Box style={{ border: '1px solid red' }}>
       <Container
-        style={{ textAlign: "center", border: "1px solid black" }}
+        style={{ textAlign: 'center', border: '1px solid black' }}
         fixed
       >
-        <h1>Kimmy's Checkbook</h1>
+        <h1>Kimmy`&apos;`s Checkbook</h1>
         <h3>Log In</h3>
         <Box mb={2} component="form" noValidate autoComplete="off">
-          <FormControl sx={{ width: "25ch" }}>
+          <FormControl sx={{ width: '25ch' }}>
             <OutlinedInput name="email" placeholder="Email" />
           </FormControl>
         </Box>
       </Container>
     </Box>
   );
-};
+}
 
 export default App;
