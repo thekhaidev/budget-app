76e8ef97f45c6115c0591da4d1c215dbb30d9fff
solo917 <kqtran917@yahoo.com>
2023-01-11 17:16:57 -0600

    Created Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+    Container,
+    Box,
+  } from '@mui/material';
+
+const Home = () => {
+    return (
+      <Box>
+          <Container>
+              <h1>
+                  Hello, world
+              </h1>
+          </Container>
+      </Box>
+    )
+}
+
+export default Home
