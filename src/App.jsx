b80491e4eab9684f28508ddf8411096257efb27d
solo917 <kqtran917@yahoo.com>
2023-01-10 23:02:09 -0600

    Added form entry section

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   Container,
   Box,
   FormControl,
-  InputLabel,
-  Input,
-  FormHelperText,
-  OutlinedInput,
+  TextField,
 } from '@mui/material';
 
 function App() {
+  const [formValue, setFormValue] = useState({ email: '', password: '' });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValue({
+      ...formValue,
+      [name]: value,
+    });
+    console.log(formValue);
+  };
+
   return (
     <Box style={{ border: '1px solid red' }}>
       <Container
         style={{ textAlign: 'center', border: '1px solid black' }}
         fixed
       >
-        <h1>Kimmy`&apos;`s Checkbook</h1>
-        <h3>Log In</h3>
-        <Box mb={2} component="form" noValidate autoComplete="off">
+        <h1>Kimmy&apos;s Checkbook</h1>
+
+        <Box mb={1} component="form" noValidate autoComplete="off">
+          <h3>Log In</h3>
           <FormControl sx={{ width: '25ch' }}>
-            <OutlinedInput name="email" placeholder="Email" />
+            <TextField
+              id="email-input"
+              name="email"
+              label="Email"
+              type="text"
+              value={formValue.email}
+              onChange={handleInputChange}
+            />
+
+            <TextField
+              margin="normal"
+              id="password-input"
+              name="password"
+              label="Password"
+              type="text"
+              value={formValue.password}
+              onChange={handleInputChange}
+            />
           </FormControl>
         </Box>
+        <Box component="span" />
+        Register for account
       </Container>
     </Box>
   );
