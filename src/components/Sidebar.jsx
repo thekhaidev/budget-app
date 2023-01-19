fc685250ed989a1153f2fd5851aa5a8363525d91
solo917 <kqtran917@yahoo.com>
2023-01-18 19:32:29 -0600

    Added select element to accounts

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,8 @@ import React from 'react'; import {
   Drawer,
   Divider,
   Box,
+  Select,
+  MenuItem,
 } from '@mui/material';
 
 const Sidebar = () => (
@@ -15,12 +17,20 @@ const Sidebar = () => (
       variant="permanent"
       anchor="left"
     >
-      <h2>Incoming</h2>
-      <h2>Outgoing</h2>
-      <Divider />
 
-      <Divider />
       <h2>Accounts</h2>
+      <Select sx={{ width: '80%', alignSelf: 'center' }}>
+        <MenuItem>Checking</MenuItem>
+        <MenuItem>Saving</MenuItem>
+      </Select>
+      <Divider />
+      <h2>Incoming</h2>
+      $23,000,000
+      <h2>Outgoing</h2>
+      $1
+      <h2>Balance</h2>
+      $22,999,999
+
     </Drawer>
   </Box>
 
