6c57b1258cd319b7bc9eaba0b1e9dd53f7d30b86
solo917 <kqtran917@yahoo.com>
2023-01-25 17:55:00 -0600

    Replaced logout button with topbar component

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   AppBar,
-  Button,
   Toolbar,
   Typography,
+  Menu,
+  MenuItem,
+  IconButton,
 
 } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 const Topbar = ({ logout }) => {
   const todayDate = new Date().toLocaleDateString('en-US');
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenu = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <AppBar
@@ -31,7 +43,36 @@ const Topbar = ({ logout }) => {
         >
           {todayDate}
         </Typography>
-        <Button onClick={() => logout()} color="inherit">logout</Button>
+        {/* <Button onClick={() => logout()} color="inherit">logout</Button> */}
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleMenu}
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem onClick={logout}>Logout</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
