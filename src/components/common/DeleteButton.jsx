a46b993cace9a0a2975b411d64ce64b450c0fca5
solo917 <kqtran917@yahoo.com>
2023-01-25 21:24:41 -0600

    Created post route for delete

--- src/components/common/DeleteButton.jsx
+++ src/components/common/DeleteButton.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import Axios from 'axios';
 
 import {
 
   Button,
 } from '@mui/material';
 
-const DeleteButton = ({ account, id }) => (
-  <Button
-    variant="outline"
-    size="small"
-    onClick={() => {
-      console.log(account, id);
-    }}
-  >
-    Delete
-  </Button>
-);
+const DeleteButton = ({ account, id }) => {
+  const handleSubmit = () => {
+    Axios.post('http://localhost:3000/delete', {})
+      .then((res) => {
+        console.log(res.data);
+      }).catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <Button
+      variant="outline"
+      size="small"
+      onClick={handleSubmit}
+    >
+      Delete
+    </Button>
+  );
+};
 
 export default DeleteButton;
