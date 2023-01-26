073b50db1a2989f035b09720934a0b9042c0ce61
solo917 <kqtran917@yahoo.com>
2023-01-25 22:14:52 -0600

    Added backdrop to Add Entry Button component

--- src/components/common/AddEntryButton.jsx
+++ src/components/common/AddEntryButton.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   Button,
+  Backdrop,
+  Box,
+  CircularProgress,
 } from '@mui/material';
 
-const AddEntryButton = ({ currentlySelected }) => (
-  <Button
-    variant="outlined"
-    onClick={() => console.log(currentlySelected)}
-  >
-    {' '}
-    Add Entry
+const AddEntryButton = ({ currentlySelected }) => {
+  const [open, setOpen] = useState(false);
 
-  </Button>
-);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <Box>
+      <Button
+        variant="outlined"
+        onClick={handleToggle}
+      >
+        {' '}
+        Add Entry
+
+      </Button>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={handleClose}
+      >
+        <Box>{currentlySelected}</Box>
+      </Backdrop>
+    </Box>
+  );
+};
 export default AddEntryButton;
